691626df50283f5c85077328f1ae495b5287c7c2
Yasir Ali <ali.yasir0@gmail.com>
2013-07-07 01:05:18 +0500

    Create publisher.php

---
+++ publisher.php
@@ -0,0 +1,13 @@
+<?php
+
+require("../phpMQTT.php");
+
+  
+$mqtt = new phpMQTT("localhost", 1883, "MQTT Publisher Client");
+
+if ($mqtt->connect()) {
+	$mqtt->publish("yasir/ali/test/topic","Hello World! at ".date("r"),0);
+	$mqtt->close();
+}
+
+?>
